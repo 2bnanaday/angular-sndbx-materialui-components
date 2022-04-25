944d8dd8c822a6916253b9799d3b6f8590d455ba
2bnanaday <2bnanaday@gmail.com>
2022-04-24 23:55:27 -0500

    add in dynamic avatars using ngStyle directive

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { contacts } from '../contacts';
 
+export type AvatarType = 'misspiggy' | 'kermitfrog';
+
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
@@ -8,6 +10,22 @@ import { contacts } from '../contacts';
 })
 export class ContactListComponent implements OnInit {
   contacts = contacts;
+
+  photo = 'https://github.com/2bnanaday/angular-sndbx-materialui-components/blob/master/src/assets/img/misspiggy.jpg?raw=true';
+
+  avatar: AvatarType = 'misspiggy';
+
+  getAvatar(type: AvatarType) {
+    this.avatar = type;
+  }
+
+  // get showListContact() {
+  //   return this.editor === 'contact-list';
+  // }
+
+  // get showAddContact() {
+  //   return this.editor === 'add-contact';
+  // }
 
   constructor() {}
 
--- src/app/contacts.ts
+++ src/app/contacts.ts
@@ -1,14 +1,16 @@
 export const contacts: any = [
   {
-    fName: 'Miss',
-    lName: 'Piggy',
+    fName: 'miss',
+    lName: 'piggy',
     phoneNumber: '701-xxx-xxxx',
-    avatarUrl:'',
+    avatarUrl:
+      'https://github.com/2bnanaday/angular-sndbx-materialui-components/blob/master/src/assets/img/misspiggy.jpg?raw=true',
   },
   {
-    fName: 'Kermit',
-    lName: 'Frog',
+    fName: 'kermit',
+    lName: 'frog',
     phoneNumber: '701-xxx-xxxx',
-    avatarUrl:'',
+    avatarUrl:
+      'https://github.com/2bnanaday/angular-sndbx-materialui-components/blob/master/src/assets/img/kermitfrog.jpg?raw=true',
   },
 ];
