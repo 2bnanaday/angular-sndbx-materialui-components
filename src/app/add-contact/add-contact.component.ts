248b85593a6e354abd667853f9678fc04fa8f0aa
2bnanaday <2bnanaday@gmail.com>
2022-04-27 00:38:38 -0500

    move chunk code from edit-contact into add-contact (bcuz using http.post method)

--- src/app/add-contact/add-contact.component.ts
+++ src/app/add-contact/add-contact.component.ts
@@ -1,21 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
-
-
-
+import { FormControl, Validators } from '@angular/forms';
+import { Contact } from '../model/contact';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
-  styleUrls: ['./add-contact.component.css']
+  styleUrls: ['./add-contact.component.css'],
 })
 export class AddContactComponent implements OnInit {
+  fNameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  lNameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
-  fNameFormControl = new FormControl('', [Validators.required, Validators.email]);lNameFormControl = new FormControl('', [Validators.required, Validators.email]);
+  contactList: Contact[] = [];
+  nextID: number = 0;
+  cfName: string;
+  clName: string;
+  cPhone: string;
+  cEmail: string;
+  cAvatarUrl: string;
 
-  constructor() { }
+  constructor(private ctService: ContactService) {}
 
   ngOnInit() {
+    this.contactList = this.ctService.CONTACTS;
   }
 
+  addNewContact() {
+    const newContact: Contact = {
+      id: ++this.nextID,
+      fName: this.cfName,
+      lName: this.clName,
+      phoneNumber: this.cPhone,
+      email: this.cEmail,
+      avatarUrl: this.cAvatarUrl,
+    };
+    this.contactList.push(newContact);
+  }
 }
--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,36 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../model/contact';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-edit-contact',
   templateUrl: './edit-contact.component.html',
-  styleUrls: ['./edit-contact.component.css']
+  styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
+  constructor() {}
 
-  contactList: Contact[] = [];
-  nextID: number = 0;
-  cfName: string;
-  clName: string;
-  cPhone: string;
-  cEmail: string;
-  cAvatarUrl: string;
-
-  constructor(private ctService: ContactService) {}
-
-  ngOnInit() {
-    this.contactList = this.ctService.CONTACTS;
-  }
-
-  addNewContact() {
-    const newContact: Contact = {
-      id: ++this.nextID,
-      fName: this.cfName,
-      lName: this.clName,
-      phoneNumber: this.cPhone,
-      email: this.cEmail,
-      avatarUrl: this.cAvatarUrl,
-    };
-    this.contactList.push(newContact);
-  }
-
+  ngOnInit() {}
 }
